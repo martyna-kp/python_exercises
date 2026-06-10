c0797f6f05ad724cb92401a3d54da7e288dc0657
martyna-kp <martyna.kapitanska@gmail.com>
2026-06-10 13:40:43 +0200

    Add a new exercise from codewars

---
+++ remove_first_last.py
@@ -0,0 +1,6 @@
+# Write a function that removes the first and last characters of a string. You're given one parameter, the original string.
+
+# Important: Your function should handle strings of any length ≥ 2 characters. For strings with exactly 2 characters, return an empty string.
+
+def remove_char(s):
+    return s[1:-1]
