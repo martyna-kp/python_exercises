f9bca126df548edfb56ff1362b470bdecdc0bc2b
martyna-kp <martyna.kapitanska@gmail.com>
2026-06-14 11:24:37 +0200

    Add a codewars exercise

---
+++ count_sheep.py
@@ -0,0 +1,4 @@
+# Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+def count_sheep(sheep):
+    return sum(1 for s in sheep if s is True)
